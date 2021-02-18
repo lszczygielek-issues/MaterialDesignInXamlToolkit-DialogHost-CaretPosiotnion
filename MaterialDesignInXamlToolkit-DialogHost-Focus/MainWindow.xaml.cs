e40bc4442ecdcd1b470b8497b64edf9c3b93dcc3
Łukasz <41156511+lszczygielek@users.noreply.github.com>
2021-02-18 03:20:24 +0100

    Initial commit

---
+++ MaterialDesignInXamlToolkit-DialogHost-Focus/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using MaterialDesignThemes.Wpf;
+using System.Windows;
+
+namespace MaterialDesignInXamlToolkit_DialogHost_Focus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel updateSourceTriggerOnPropertyChangedViewModel;
+        private ViewModel updateSourceTriggerOnLostFocusViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            updateSourceTriggerOnLostFocusViewModel = new ViewModel();
+            OnLostFocusExample.DataContext = updateSourceTriggerOnLostFocusViewModel;
+
+            updateSourceTriggerOnPropertyChangedViewModel = new ViewModel();
+            OnPropertyChangedExample.DataContext = updateSourceTriggerOnPropertyChangedViewModel;
+        }
+
+        private async void OnLostFocusExampleButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await DialogHost.Show(
+                new ViewModel { LastInput = updateSourceTriggerOnLostFocusViewModel.LastInput },
+                "OnLostFocusExample",
+                (object sender, DialogClosingEventArgs args) =>
+                {
+                    if (args.Parameter is bool isConfirmed && !isConfirmed)
+                    {
+                        return;
+                        
+                    }
+                    else
+                    {
+                        updateSourceTriggerOnLostFocusViewModel.LastInput = ((ViewModel)args.Session.Content).LastInput;
+                    }
+                });
+        }
+
+        private async void OnPropertyChangedExampleButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await DialogHost.Show(
+                new ViewModel { LastInput = updateSourceTriggerOnPropertyChangedViewModel.LastInput },
+                "OnPropertyChangedExample",
+                (object sender, DialogClosingEventArgs args) =>
+                {
+                    if (args.Parameter is bool isConfirmed && !isConfirmed)
+                    {
+                        return;
+
+                    }
+                    else
+                    {
+                        updateSourceTriggerOnPropertyChangedViewModel.LastInput = ((ViewModel)args.Session.Content).LastInput;
+                    }
+                });
+        }
+    }
+}
---
+++ MaterialDesignInXamlToolkit-DialogHost-Focus/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MaterialDesignInXamlToolkit_DialogHost_Focus
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string lastInput;
+
+        public string LastInput 
+        { 
+            get => lastInput;
+
+            set
+            {
+                lastInput = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
